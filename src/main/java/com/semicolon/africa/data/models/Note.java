ab18693dff7a2cecdfc96904fa476e73dab8b339
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-08-14 09:00:29 +0100

    first commit

--- src/main/java/com/semicolon/africa/data/models/Note.java
+++ src/main/java/com/semicolon/africa/data/models/Note.java
@@ -3,11 +3,13 @@ package com.semicolon.africa.data.models;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@Document("Note")
 public class Note {
     @Id
     private String id;
--- src/main/java/com/semicolon/africa/data/models/User.java
+++ src/main/java/com/semicolon/africa/data/models/User.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Setter
 @Getter
+@Document("User")
 public class User {
     @Id
     private String id;
